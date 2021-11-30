c03d5a2bd6bd2f5caaf493a0a5243ec42e36183d
eulucasm <lucaspoletis@gmail.com>
2021-11-30 00:51:27 -0300

    complete template

--- src/index.js
+++ src/index.js
@@ -5,4 +5,30 @@ const {
 
 const app = express();
 
+app.use(express.json());
+
+app.post('/users', (request, response) => {
+
+});
+
+app.get('/todos', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.post('/todos', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
+app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
+  // Complete aqui
+});
+
 module.exports = app;
